18ed4f5dc19e722802d4bc00078f52168135cc69
nahlabalabaki <nahla.baalbaki@lau.edu>
2022-02-04 14:11:55 +0200

    Added Main Activity-Edited

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     RadioGroup radiogroup;
     RadioButton radiobutton1;
     RadioButton radiobutton2;
+    EditText number;
 
 
     @Override
@@ -21,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView number;
         number = findViewById(R.id.num);
         radiobutton1=findViewById(R.id.r1);
         radiobutton2=findViewById(R.id.r2);
