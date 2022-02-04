3c8a1f0682d2a7d0effc9069fab14e04bf9e6ae6
nahlabalabaki <nahla.baalbaki@lau.edu>
2022-02-04 09:40:33 +0200

    Added Main Activity

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,13 +3,56 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    RadioGroup radiogroup;
+    RadioButton radiobutton1;
+    RadioButton radiobutton2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView number;
+        number = findViewById(R.id.num);
+        radiobutton1=findViewById(R.id.r1);
+        radiobutton2=findViewById(R.id.r2);
+
+        Button buttonApply = findViewById(R.id.button_apply);
+
+        buttonApply.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Double total;
+                Double amount = Double.parseDouble(number.toString());
+
+                if (radiobutton1.isChecked()) {
+
+                    total = amount * 22000;
+                    Toast.makeText(getApplicationContext(), total.toString(), Toast.LENGTH_SHORT).show();
+                }
+
+                else if (radiobutton2.isChecked()) {
+
+                    total = amount / 22000;
+                    Toast.makeText(getApplicationContext(), total.toString(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }
+        );
+
     }
-    
+
 }
+
+
